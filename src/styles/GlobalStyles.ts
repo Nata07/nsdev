5a5f71aa36ecc20cd6cf1b232d47ba11f85cc363
Natanael <nata.mw@hotmail.com>
2021-03-05 10:42:12 -0300

    fix: changes styled-components globals

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
 * {
   margin: 0;
   padding: 0;
@@ -96,3 +99,4 @@ span.current-experience {
 
 /* react */
 /*  #rumoaoproximonivel */
+`;
